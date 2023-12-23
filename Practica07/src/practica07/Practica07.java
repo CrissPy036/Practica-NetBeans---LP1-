73abd25b5bf9753a757aa3190cf112a3ee7c71f3
Aspire5738z <gonzalez.cristhian0136@gmail.com>
2023-12-22 21:54:29 -0300

    Pratica 7 y 8 NetBeans

---
+++ Practica07/src/practica07/Practica07.java
@@ -0,0 +1,17 @@
+
+package practica07;
+
+/**
+ * Práctica 07 - Clase 19-12-2023
+ * @author Cristhian Abel Aguilar Gonzalez
+ */
+public class Practica07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Practica08/src/practica08/Practica08.java
@@ -0,0 +1,20 @@
+
+package practica08;
+
+import Practica08Swing.frmEncuesta;
+
+/**
+ *
+ * @author Cristhian Abel Aguilar Gonzalez --- 20/12/2023
+ */
+public class Practica08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmEncuesta formulario = new frmEncuesta();
+        formulario.setVisible(true);
+    }
+    
+}
